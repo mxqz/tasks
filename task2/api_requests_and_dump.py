eaeabca406260081426b1330c66aaa5c05e0d77c
JXRK <stasorogo@gmail.com>
2025-04-15 01:01:39 +0300

    Experimental Stuff
    K

--- task2/api_requests_and_dump.py
+++ task2/api_requests_and_dump.py
@@ -1,9 +1,14 @@
 import requests
 import json
 import os
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 import tldextract as tld
 import pandas as pd
+
+
+class RequestError(Exception):
+    def __init__(self):
+        super().__init__("Request Failure")
 
 
 def dump_json_shortcut(data: dict | list, file_name: str, script_dir: str = "", sort: bool = False) -> None:
@@ -29,6 +34,7 @@ def gather_json_from(url: str):
     if response.status_code != 200:
         domain = tld.extract(url).domain
         print(f"Не вдалося отримати дані {domain}")
+        raise RequestError
     
     return response.json()
 
@@ -38,7 +44,7 @@ def gather_and_dump(url: str, script_dir: str ="") -> None:
     if not isinstance(data, list):
         data = [data]
     domain = tld.extract(url).domain
-    dump_json_shortcut(data, f"exchange_rates_{domain}", script_dir)
+    dump_json_shortcut(data, f"{domain}", script_dir)
 
 
 df_columns = ["bank", "date", "currency", "rate_buy", "rate_sell"]
@@ -115,36 +121,69 @@ def parse_json_to_df(path: str, dataframe_method) -> pd.DataFrame:
     data = json.load(open(path, "r", encoding="utf-8"))
     return dataframe_method(data)
 
-# data[data.isna().any(axis=1)]
 
-# nbu_url = f"https://bank.gov.ua/NBU_Exchange/exchange_site?start={(date.today() - timedelta(7)).strftime("%Y%m%d")}&end={date.today().strftime("%Y%m%d")}&json"
-# mono_url = f"https://api.monobank.ua/bank/currency"
-# privat_url = f"https://api.privatbank.ua/p24api/exchange_rates?json&date={date.today().strftime("%d.%m.%Y")}"
+def sorted_unique(df: pd.Series) -> list:
+    return sorted(df.unique())
 
-# # urls = [nbu_url, privat_url, mono_url]
 
-# # for url in urls:
-# #     gather_and_dump(url)
+locals = json.load(open("config\\en.json", "r"))
+url_dict = json.load(open("config\\url.json", "r"))
+bank_names = json.load(open("bank_names\\en.json", "r"))
+currency_names = json.load(open("exchange_names\\en.json", "r"))
+bank_dict = dict(zip(url_dict.keys(), [
+    dataframe_bank,
+    dataframe_monobank,
+    dataframe_privatbank
+]))
 
-bank_dict = {
-    "": dataframe_bank,
-    "mono": dataframe_monobank,
-    "privat": dataframe_privatbank
-}
+data = pd.read_csv("csv\\raw_data.csv")
+
+data["date"] = pd.to_datetime(data["date"], format="%d.%m.%Y")
+data["bank"] = data["bank"].apply(lambda name: bank_names.get(name, None))
+data["currency"] = data["currency"].apply(lambda code: currency_names.get(code, None))
+
+data.dropna(inplace=True)
+
+
+def parse_jsons_to_csv(bank_dict: dict = bank_dict, file_name: str = "raw_data.csv") -> None:
+    info = pd.concat([parse_json_to_df(f"exchange_rates\\{bank}.json", bank_dict[bank]) for bank in bank_dict], 
+                     join="inner", ignore_index=True)
+    
+    if not file_name.endswith(".csv"):
+        file_name += ".csv"
+
+    path = f"csv\\{file_name}"
+    
+    if open(path, "r"):
+        info = pd.concat([info, pd.read_csv(path)], join="inner", ignore_index=True).drop_duplicates()
+    
+    info.dropna(inplace=True)
+    
+    info.to_csv(path, index=False)
+
+
+def request_jsons_for_dates(start: datetime, end: datetime) -> None:  
+    gather_and_dump(url_dict["bank"].format(start.strftime("%Y%m%d"), end.strftime("%Y%m%d")), "exchange_names")
+
+    gather_and_dump(url_dict["monobank"], "exchange_names")
+
+    days = (end - start).days
+    privat_file = tld.extract(url_dict["privatbank"]).domain
+    privat_info = []
+    for date in [start + timedelta(day) for day in range(days)]:
+        try:
+            info = gather_json_from(url_dict["privatbank"].format(date.strftime("%d.%m.%Y")))
+        except RequestError:
+            break
+        else:
+            privat_info.append(info)
+    dump_json_shortcut(privat_info, privat_file, "exchange_rates")
+    
+
+def request_external_csv_update(start: datetime, end: datetime, file_name: str = "raw_data.csv") -> None:
+    request_jsons_for_dates(start, end)
+    parse_jsons_to_csv(file_name=file_name)
+
 
 if __name__ == "__main__":
-    # data = pd.concat([parse_json_df(f"exchange_rates\\exchange_rates_{bank}bank.json", bank_dict[bank]) for bank in bank_dict], join="inner", ignore_index=True)
-    # data.to_csv("raw_data.csv", index=False)
-
-    # currencies: dict = json.load(open("exchange_names\\ua.json", "r"))
-    # bank_names: dict = json.load(open("bank_names\\ua.json", "r"))
-
-    # data = pd.read_csv("csv\\raw_data.csv")
-    # data["currency"] = data["currency"].apply(lambda code: currencies.get(code, None))
-    # data["bank"] = data["bank"].apply(lambda bank: bank_names.get(bank, None))
-    # data.dropna(inplace=True)
-
-    # print(*sorted(data["currency"].unique()), sep="\n", end="\n\n")
-    # print(*sorted(data["bank"].unique()), sep="\n", end="\n\n")
-
-    pass
+    request_external_csv_update(datetime.strptime("01.04.2024", "%d.%m.%Y"), datetime.strptime("14.04.2025", "%d.%m.%Y"))
--- task2/menu.py
+++ task2/menu.py
@@ -1,21 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QDateEdit, QPushButton, QComboBox
 from PyQt6.QtCore import QDate
+import api_requests_and_dump as api
 import pyqtgraph as pg
-import json
 
 class CryptoExchangeGUI(QMainWindow):
-    def __init__(self, df):
+    def __init__(self):
         super().__init__()
-        self.data = df
-        self.locals = json.load(open("config\\en.json", "r"))
-        
-        self.setWindowTitle(locals["window"])
+        self.isAxisBottomUpdated = True
+        self.setWindowTitle(api.locals["window"])
         self.setGeometry(100, 100, 800, 600)
 
         self.darkTheme = True  # By default, use dark theme
 
-        self.setStyleSheet("background-color: black; color: white;")  # Dark theme by default
+        self.setStyleSheet("background-color: #000000; color: #ffffff;")  # Dark theme by default
 
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
@@ -25,18 +23,17 @@ class CryptoExchangeGUI(QMainWindow):
         self.leftPanel = QVBoxLayout()
 
         self.comboBank = QComboBox()
-        self.comboBank.addItems(sorted(self.data["bank"].unique()))
-        self.comboBank.setObjectName("comboBank")
-        self.comboBank.setStyleSheet("font-size: 14px; border: 2px solid white; border-radius: 5px; padding: 5px;")
+        self.comboBank.addItems(api.sorted_unique(api.data["bank"]))
+        # self.comboBank.setStyleSheet("font-size: 14px; border: 2px solid white; border-radius: 5px; padding: 5px;")
         self.comboBank.currentIndexChanged.connect(self.updateSelectionCurrency)
-        self.leftPanel.addWidget(QLabel(self.locals["choose"]["bank"]))
+        self.leftPanel.addWidget(QLabel(api.locals["choose"]["bank"]))
         self.leftPanel.addWidget(self.comboBank)
 
         self.comboCurrency = QComboBox()
-        self.comboCurrency.addItems(sorted(self.data[self.data["bank"] == self.comboBank.currentText()]["currency"].unique()))
-        self.comboCurrency.setStyleSheet("font-size: 14px; border: 2px solid white; border-radius: 5px; padding: 5px;")
+        self.comboCurrency.addItems(api.sorted_unique(api.data[api.data["bank"] == self.comboBank.currentText()]["currency"]))
+        # self.comboCurrency.setStyleSheet("font-size: 14px; border: 2px solid white; border-radius: 5px; padding: 5px;")
         self.comboCurrency.currentIndexChanged.connect(self.updatePlot)
-        self.leftPanel.addWidget(QLabel(self.locals["choose"]["currency"]))
+        self.leftPanel.addWidget(QLabel(api.locals["choose"]["currency"]))
         self.leftPanel.addWidget(self.comboCurrency)
 
         self.pickerDateStart = QDateEdit()
@@ -44,74 +41,35 @@ class CryptoExchangeGUI(QMainWindow):
         self.pickerDateEnd = QDateEdit()
         self.setupPicker(self.pickerDateEnd, QDate.currentDate())
 
-        self.leftPanel.addWidget(QLabel(self.locals["date"]["start"]))
+        self.leftPanel.addWidget(QLabel(api.locals["date"]["start"]))
         self.leftPanel.addWidget(self.pickerDateStart)
-        self.leftPanel.addWidget(QLabel(self.locals["date"]["end"]))
+        self.leftPanel.addWidget(QLabel(api.locals["date"]["end"]))
         self.leftPanel.addWidget(self.pickerDateEnd)
-
-        # self.update_button = QPushButton(self.locals["update"])
-        # self.update_button.clicked.connect(self.update_chart)
-        # self.update_button.setStyleSheet("""
-        #     QPushButton {
-        #         border: 2px solid white; 
-        #         border-radius: 5px; 
-        #         padding: 5px;
-        #         background-color: #333; 
-        #         color: white;
-        #     }
-        #     QPushButton:hover {
-        #         background-color: #555;
-        #     }
-        # """)
+        
+        # self.update_button = QPushButton(api.locals["update"])
+        # self.update_button.clicked.connect(self.updatePlot)
         # self.update_button.setFixedSize(120, 35)
         # self.leftPanel.addWidget(self.update_button)
 
-        # self.reset_button = QPushButton(self.locals["reset"])
+        # self.reset_button = QPushButton(api.locals["reset"])
         # self.reset_button.clicked.connect(self.reset_filters)
-        # self.reset_button.setObjectName("reset_button")
-        # self.reset_button.setStyleSheet("""
-        #     #reset_button {
-        #         border: 2px solid white; 
-        #         border-radius: 5px; 
-        #         padding: 5px;
-        #         background-color: #900; 
-        #         color: white;
-        #     }
-        #     #reset_button:hover {
-        #         background-color: #c00;
-        #     }
-        # """)
         # self.reset_button.setFixedSize(120, 35)
         # self.leftPanel.addWidget(self.reset_button)
 
-        self.leftPanel.addStretch()  # Для вирівнювання елементів вгору
+        self.leftPanel.addStretch()  # For top alignment
         self.mainLayout.addLayout(self.leftPanel)
 
         # Right panel for plot
         self.plotWidget = pg.PlotWidget()
-        self.plotWidget.setBackground("black")
-        self.plotWidget.setStyleSheet("border: 2px solid white;")
+        # self.plotWidget.setBackground("black")
         self.plotWidget.showGrid(x=True, y=True)
-        self.mainLayout.addWidget(self.plotWidget)
-
-        # Theme toggle button
-        self.buttonTheme = QPushButton(self.locals["theme"]["light"])
-        self.buttonTheme.clicked.connect(self.toggleTheme)
-        self.buttonTheme.setStyleSheet("""
-            QPushButton {
-                border: 2px solid white;
-                border-radius: 5px;
-                padding: 5px;
-                background-color: #333;
-                color: white;
-            }
-            QPushButton:hover {
-                background-color: #555;
-            }
-        """)
-        self.leftPanel.addWidget(self.buttonTheme)
-
         self.updatePlot()
+        self.mainLayout.addWidget(self.plotWidget)
+        
+        # Theme toggle button
+        self.buttonTheme = QPushButton(api.locals["theme"]["light"])
+        self.buttonTheme.clicked.connect(self.toggleTheme)
+        self.leftPanel.addWidget(self.buttonTheme)
 
     def setupPicker(self, picker: QDateEdit, date: QDate):
         picker.setCalendarPopup(True)
@@ -119,98 +77,14 @@ class CryptoExchangeGUI(QMainWindow):
         picker.dateChanged.connect(self.updatePlot)
 
     def toggleTheme(self):
-        if self.darkTheme:
-            self.setStyleSheet("""
-                background-color: #f0f0f0; 
-                color: black;
-            """)
-            self.plotWidget.setBackground('#ffffff')
-            # self.update_button.setStyleSheet("""
-            #     QPushButton {
-            #         border: 2px solid black;
-            #         border-radius: 5px;
-            #         padding: 5px;
-            #         background-color: #ccc;
-            #         color: black;
-            #     }
-            #     QPushButton:hover {
-            #         background-color: #bbb;
-            #     }
-            # """)
-            # self.reset_button.setStyleSheet("""
-            #     QPushButton {
-            #         border: 2px solid black;
-            #         border-radius: 5px;
-            #         padding: 5px;
-            #         background-color: #e94e77;
-            #         color: black;
-            #     }
-            #     QPushButton:hover {
-            #         background-color: #d42f60;
-            #     }
-            # """)
-            self.buttonTheme.setStyleSheet("""
-                QPushButton {
-                    border: 2px solid black;
-                    border-radius: 5px;
-                    padding: 5px;
-                    background-color: #ccc;
-                    color: black;
-                }
-                QPushButton:hover {
-                    background-color: #bbb;
-                }
-            """)
-        else:
-            self.setStyleSheet("""
-                background-color: black;
-                color: white;
-            """)
-            self.plotWidget.setBackground('black')
-            # self.update_button.setStyleSheet("""
-            #     QPushButton {
-            #         border: 2px solid white;
-            #         border-radius: 5px;
-            #         padding: 5px;
-            #         background-color: #333;
-            #         color: white;
-            #     }
-            #     QPushButton:hover {
-            #         background-color: #555;
-            #     }
-            # """)
-            # self.reset_button.setStyleSheet("""
-            #     QPushButton {
-            #         border: 2px solid white;
-            #         border-radius: 5px;
-            #         padding: 5px;
-            #         background-color: #900;
-            #         color: white;
-            #     }
-            #     QPushButton:hover {
-            #         background-color: #c00;
-            #     }
-            # """)
-            self.buttonTheme.setStyleSheet("""
-                QPushButton {
-                    border: 2px solid white;
-                    border-radius: 5px;
-                    padding: 5px;
-                    background-color: #333;
-                    color: white;
-                }
-                QPushButton:hover {
-                    background-color: #555;
-                }
-            """)
-
         theme = "dark" if self.darkTheme else "light"
-        self.buttonTheme.setText(self.locals["theme"][theme])
+        self.buttonTheme.setText(api.locals["theme"][theme])
+        # self.setStyleSheet(open(f"config\\themes\\{theme}.qss").read())
         self.darkTheme = not self.darkTheme
         self.updatePlot()  # Re-draw the chart with the new theme
 
-    def addPlot(self, widget: pg.PlotWidget, x: list, y: list, color: str, name: str):
-        widget.plot(list(range(len(x))), y,
+    def addPlot(self, widget: pg.PlotWidget, y: list, color: str, name: str):
+        widget.plot(list(range(len(y))), y,
                     pen=pg.mkPen(color=color, width=2),
                     symbol='o', symbolBrush=color, symbolPen=None, name=name)
 
@@ -221,34 +95,45 @@ class CryptoExchangeGUI(QMainWindow):
         end = self.pickerDateEnd.date().toPyDate()
 
         if start > end:
+            self.pickerDateEnd.setDate(self.pickerDateStart.date())
             return
 
-        selection = self.data[
-            (self.data["bank"] == selectedBank) &
-            (self.data["currency"] == selectedCurrency) &
-            (self.data["date"].dt.date.between(start, end))]
+        selection = api.data[
+            (api.data["bank"] == selectedBank) &
+            (api.data["currency"] == selectedCurrency) &
+            (api.data["date"].dt.date.between(start, end))]
         
         # rates = [sum(i) / len(i) for i in zip(selection["rate_buy"], selection["rate_sell"])]
-        rates_buy = list(selection["rate_buy"])
-        rates_sell = list(selection["rate_sell"])
+        rates_buy = selection["rate_buy"].tolist()
+        rates_sell = selection["rate_sell"].tolist()
 
-        labels = selection["date"].apply(lambda date: date.strftime("%d.%m"))
+        dates = selection["date"].apply(lambda date: date.strftime("%d.%m")).tolist()
+
+        # self.plotWidget.setLabel("left", api.locals["exchange"].format(selectedCurrency), color='r')
+        # self.plotWidget.setLabel("bottom", api.locals["plot"]["bottom"], color='r')
 
         self.plotWidget.clear()
-        self.addPlot(self.plotWidget, labels, rates_buy, 'g', name=f"{selectedBank} Buy Rate")
-        self.addPlot(self.plotWidget, labels, rates_sell, 'g', name=f"{selectedBank} Sell Rate")
-        
-        # self.plotWidget.setLabel("left", self.locals["exchange"].format(selectedCurrency), color='r')
-        # self.plotWidget.setLabel("bottom", self.locals["plot"]["bottom"], color='r')
-        self.plotWidget.setTitle(self.locals["plot"]["title"].format(selectedBank), color='r')
-        self.plotWidget.getPlotItem().getAxis("bottom").setTicks([list(enumerate(labels))])
+        self.plotWidget.setTitle(api.locals["plot"]["title"].format(selectedBank), color="#808080")
         self.plotWidget.addLegend()
+
+        self.addPlot(self.plotWidget, rates_buy, color="#808080", name=api.locals["plot"]["legend"]["buy"])
+        self.addPlot(self.plotWidget, rates_sell, color="#808080", name=api.locals["plot"]["legend"]["sell"])
+
+        if not len(dates):
+            self.isAxisBottomUpdated = False
+        
+        if len(dates) and not self.isAxisBottomUpdated:
+            self.isAxisBottomUpdated = True
+            self.plotWidget.getPlotItem().setAxisItems({"bottom": pg.AxisItem(orientation="bottom")})
+
+        self.plotWidget.getPlotItem().getAxis("bottom").setTicks([list(enumerate(dates))])
+        
         self.plotWidget.plotItem.vb.autoRange()
+        
 
     def updateSelectionCurrency(self):
-        currencies = sorted(self.data[self.data["bank"] == self.comboBank.currentText()]["currency"].unique())
         self.comboCurrency.clear()
-        self.comboCurrency.addItems(currencies)
+        self.comboCurrency.addItems(api.sorted_unique(api.data[api.data["bank"] == self.comboBank.currentText()]["currency"]))
         self.comboCurrency.setCurrentIndex(0)
         self.updatePlot()
 
@@ -258,10 +143,3 @@ class CryptoExchangeGUI(QMainWindow):
     #     self.comboBank.setCurrentIndex(0)
     #     self.comboCurrency.setCurrentIndex(0)
     #     self.updateChart()
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = CryptoExchangeGUI()
-    window.show()
-    sys.exit(app.exec())
