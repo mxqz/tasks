17a0620a143bf73ab3fc5a98abc1c66961a35df4
mxqz <mxasd@ukr.net>
2025-04-29 20:20:38 +0300

    tokens

--- task3/compiler.py
+++ task3/compiler.py
@@ -1,3 +1,5 @@
+from tokens import Token, KEYWORDS, OPERATORS, SEPARATORS
+
 program = ""
 
 with open("program.txt", "r") as file:
@@ -8,23 +10,57 @@ with open("program.txt", "r") as file:
 program = program.replace(" ", "")
 program = program.replace("\n", "")
 
+tokens = []
+current = ""
+
 print(program, end="\n\n")
 
-tokens = []
-temp = ""
+def add_token(value):
+    """Функція для визначення типу токена"""
+    if not value:
+        return
 
-for i in program:
-    match i:
-        case ";" | "(" | ")" | "[" | "]" | "{" | "}" | "=" | "+" | "-" | "*" | "/":
-            if temp:
-                tokens.append(temp)
-            tokens.append(i)
-            temp = ""
-        case ">":
-            temp += i
-            tokens.append(temp)
-            temp = ""
-        case _:
-            temp += i
+    if value in KEYWORDS:
+        tokens.append(Token("KEYWORD", value))
+    elif value in OPERATORS:
+        tokens.append(Token("OPERATOR", value))
+    elif value in SEPARATORS:
+        tokens.append(Token("SEPARATOR", value))
+    elif value.isidentifier():
+        tokens.append(Token("IDENTIFIER", value))
+    elif value.isdigit():
+        tokens.append(Token("NUMBER", int(value)))
+    else:
+        try:
+            float_val = float(value)
+            tokens.append(Token("NUMBER", float_val))
+        except ValueError:
+            raise ValueError(f"Uknown token: {value}")
 
-print(*tokens, end="\n\n")
+
+i = 0
+while i < len(program):
+    ch = program[i]
+
+    if ch in OPERATORS or ch in SEPARATORS:
+        add_token(current)
+        current = ""
+        tokens.append(Token(
+            "OPERATOR" if ch in OPERATORS else "SEPARATOR", ch
+        ))
+        i += 1
+        continue
+
+    if ch == " ":
+        add_token(current)
+        current = ""
+        i += 1
+        continue
+
+    current += ch
+    i += 1
+
+add_token(current)
+
+for token in tokens:
+    print(token)
